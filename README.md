3dd2ab7b0ebea83eb59d46b5ecae5a76427d4d5c
tony/gaming laptop upstairs <tonetheman@gmail.com>
2020-05-12 12:53:29 -0500

    added v2

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Saw this https://www.youtube.com/watch?v=ih20l3pJoeU
 
 Following along with love2d
+
+v2 - starting at the source here and seeing if I can get it to work
+https://github.com/OneLoneCoder/videos/blob/master/OneLoneCoder_olcEngine3D_Part1.cpp
+
+v1 - my attempt to follow video (not really working so well)
---
+++ v2/main.lua
@@ -0,0 +1,62 @@
+W = 640
+H = 480
+
+local Vec3d = {}
+function Vec3d.new(x,y,z)
+    local self = {x=x,y=y,z=z}
+    return self
+end
+Vec3d.ZERO = Vec3d.new(0,0,0)
+function Vec3d.newZero()
+    return Vec3d.new(0,0,0)
+end
+
+local Triangle = {}
+function Triangle()
+    local self = {}
+    self[1] = Vec3d.newZero()
+    self[2] = Vec3d.newZero()
+    self[3] = Vec3d.newZero()
+    return self
+end
+
+local Mesh = {}
+function Mesh.new()
+    local self = {}
+    return self
+end
+
+local mat4x4 = { m = {
+    {0,0,0,0},
+    {0,0,0,0},
+    {0,0,0,0},
+    {0,0,0,0}
+    }
+}
+
+--[[
+    take an input vector - i
+    take an input matrix of type mat4x4
+]]
+function MultiplyMatrixVector(i,m)
+    local o = Vec3d.newZero()
+    
+    o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+    o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+    o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+    local w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+    if w ~= 0.0 then
+        o.x = o.x/w
+        o.y = o.y/w
+        o.z = o.z/w
+    end
+    return o
+end
+
+
+function love.load()
+end
+
+function love.draw()
+end
