6b502c5b9b2d52fd4c5d33aec68e7f02f1a3e472
tony/gaming laptop upstairs <tonetheman@gmail.com>
2020-05-12 13:53:27 -0500

    lots of code added

--- v2/main.lua
+++ v2/main.lua
@@ -3,6 +3,11 @@
 local Vec3d = {}
 function Vec3d.new(x,y,z)
     local self = {x=x,y=y,z=z}
+    function self:copyinto(other)
+        self.x = other.X
+        self.y = other.y
+        self.z = other.z
+    end
     return self
 end
 Vec3d.ZERO = Vec3d.new(0,0,0)
@@ -16,6 +21,13 @@ function Triangle()
     self.p[1] = Vec3d.newZero()
     self.p[2] = Vec3d.newZero()
     self.p[3] = Vec3d.newZero()
+
+    function self:copyinto(other)
+        self.p[1]:copyinto(other.p[1])
+        self.p[2]:copyinto(other.p[2])
+        self.p[3]:copyinto(other.p[3])
+    end
+
     return self
 end
 
@@ -90,6 +102,10 @@ function love.load()
 end
 
 function love.update(dt)
+    fTheta = fTheta + dt
+end
+
+function love.draw()
     local matRotZ = Mat4x4.new()
     matRotZ.m[1][1] = math.cos(fTheta)
     matRotZ.m[1][2] = math.sin(fTheta);
@@ -108,8 +124,52 @@ function love.update(dt)
     for i,tri in ipairs(mesh) do
         local triRotatedZ = Triangle.new()
 
+        -- rotate in Z axis
         triRotatedZ.p[1] = MultiplyMatrixVector(tri.p[1], matRotZ)
         triRotatedZ.p[2] = MultiplyMatrixVector(tri.p[2], matRotZ);
 		triRotatedZ.p[3] = MultiplyMatrixVector(tri.p[3], matRotZ);
+
+        -- rotate in X axis
+        local triRotatedZX = Triangle.new()
+        triRotatedZX.p[1] = MultiplyMatrixVector(triRotatedZ.p[1], matRotX);
+        triRotatedZX.p[2] = MultiplyMatrixVector(triRotatedZ.p[2], matRotX);
+        triRotatedZX.p[3] = MultiplyMatrixVector(triRotatedZ.p[3], matRotX);
+
+        -- offset into the screen
+        local triTranslated = Triangle.new() -- makes a new one
+        -- triTranslated = triRotatedZX;
+        triTranslated:copyinto(triRotatedZX)
+
+        triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0;
+        triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0;
+        triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0;
+
+        -- project from 3d to 2d now!!!
+        local triProjected = Triangle.new()
+        triProjected.p[1] = MultiplyMatrixVector(triTranslated.p[1], matProj);
+        triProjected.p[2] = MultiplyMatrixVector(triTranslated.p[2], matProj);
+        triProjected.p[3] = MultiplyMatrixVector(triTranslated.p[3], matProj);
+
+        -- scale into view
+        triProjected.p[1].x = triProjected.p[1].x + 1.0; 
+        triProjected.p[1].y = triProjected.p[1].y +  1.0;
+        triProjected.p[2].x = triProjected.p[2].x + 1.0
+        triProjected.p[2].y = triProjected.p[2].y +  1.0;
+        triProjected.p[3].x = triProjected.p[3].x + 1.0; 
+        triProjected.p[3].y = triProjected.p[3].y + 1.0;
+        triProjected.p[1].x = triProjected.p[1].x * 0.5 * gameData.W
+        triProjected.p[1].y = triProjected.p[1].y * 0.5 * gameData.H
+        triProjected.p[2].x = triProjected.p[2].x * 0.5 * gameData.W
+        triProjected.p[2].y = triProjected.p[2].y * 0.5 * gameData.H
+        triProjected.p[3].x = triProjected.p[3].x * 0.5 * gameData.W
+        triProjected.p[3].y = triProjected.p[3].y * 0.5 * gameData.H
+
+
+        love.graphics.polygon("line", 
+            triProjected.p[1].x, triProjected.p[1].y,
+            triProjected.p[2].x, triProjected,p[2].y,
+            triProjected.p[3].x, triProjected.p[3].y
+    )
     end
 end
+
